334e70461f2f818e80256ff1c01da4e26f859ef2
Nicolas Castañeda <nickcar13@gmail.com>
2020-02-22 21:53:34 -0500

    Raycast y Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,20 +9,54 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 6f;
     Rigidbody2D playerRB;
 
+    public LayerMask groundmask;
+
+    void Awake()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+    }
+
+    //* Realiza el salto
+    void Jump()
+    {
+        if (IsTouchingTheGround())
+        {
+            playerRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
 
     }
 
-    void Jump()
+    //* Nos indica si esta el personaje tocando o no el suelo con raycast
+    bool IsTouchingTheGround()
     {
-
+        //* inicio las fisicas para trazar el rayo invisible
+        if (Physics2D.Raycast(this.transform.position, //* desde donde sale el rayo en este caso el personaje
+            Vector2.down, //* hacia donde sale el rayo
+            1.5f, //* distancia maxima
+            groundmask)) //* contra la mascara del suelo
+        {
+            //TODO: programar logica de contacto con el suelo
+            return true;
+        }
+        else
+        {
+            //TODO: profrmar logica de noo contacto
+            return false;
+        }
     }
 }
