082ad27d01473fbb18d2a26e9f294203e9bdf39d
Nicolas Castañeda <nickcar13@gmail.com>
2020-02-22 20:34:29 -0500

    El script de control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //* variables del movimiento del personaje
+
+    public float jumpForce = 6f;
+    Rigidbody2D playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Jump()
+    {
+
+    }
+}
